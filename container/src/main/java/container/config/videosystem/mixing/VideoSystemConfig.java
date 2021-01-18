6ea55815c8fab986a96eea278691879710335337
jiyoung <jiyoung4061@naver.com>
2021-01-18 16:32:51 +0900

    Explicit Java Mixing Configuration

---
+++ container/src/main/java/container/config/videosystem/mixing/VideoSystemConfig.java
@@ -0,0 +1,22 @@
+package container.config.videosystem.mixing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 
+ * @author javafullstack-bitacademy
+ * 
+ * Explicit Configuration : Java Configuration
+ *     JavaConfig3	   <------------   JavaConfig1	+	JavaConfig2
+ * (VideoPlayerConfig)	   [Import]	   (DVDConfig)	  (DVDPlayerConfig)
+ *
+ * JavaConfig3 = javaConfig2 + JavaConfig1 
+ * 
+ */
+
+@Configuration
+@Import({DVDConfig.class, DVDPlayerConfig.class})
+public class VideoSystemConfig {
+	
+}
